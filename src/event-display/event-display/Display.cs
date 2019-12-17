b9f780ec541dd6c15939727e0cf9f7a4d98b3909
Bruno Fillion <brunofillion@Mac-Pro-de-Bruno.local>
2019-12-17 12:27:06 -0500

    v1

--- src/event-display/event-display/Display.cs
+++ src/event-display/event-display/Display.cs
@@ -1,10 +1,24 @@
 ﻿using System;
+using Serilog;
+
 namespace event_display
 {
     public class Display
     {
         public Display()
         {
+        }
+
+        public string DisplayMessage(string message)
+        {
+            if (message == "")
+            {
+                message = "{\"msg\": \"No data!\"}";
+            }
+
+            Log.Information("Event Display received event: {content}", message);
+
+            return message;
         }
     }
 }
